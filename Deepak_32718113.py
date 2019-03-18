10824e87c43afd75853af1c18ee45c36b4436c8e
Deepak Balaji <25303471+Deepak710@users.noreply.github.com>
2019-03-18 07:29:37 +0530

    Add files via upload

--- Deepak_32718113.py
+++ Deepak_32718113.py
@@ -21,6 +21,9 @@ while True:
                 break
         time_list = list()
         file_list = list()
+        access_list = list()
+        mod_list = list()
+        size_list = list()
         for p,s,f in os.walk(root_path):
             print("\nCurrently Searching in ", p)
             for i in f:
@@ -30,6 +33,9 @@ while True:
                     print(i, "\twas created on\t", ctime(file_time))
                     time_list.append(file_time)
                     file_list.append(file_name)
+                    access_list.append(ctime(os.path.getatime(file_name)))
+                    mod_list.append(ctime(os.path.getmtime(file_name)))
+                    size_list.append(os.path.getsize(file_name))
         if len(time_list):
             save = input("\nDo you want to save this result in an Excel Workbook? (y/n)")
             if save == 'y' or save == 'Y':
@@ -41,16 +47,22 @@ while True:
                 ws.write(0, 0, "Date of the Exercise : {0}".format(ctime(today.timestamp())))
                 ws.write(1, 0, "Date {0} days ago : {1}".format(days, ctime(diff))) 
                 ws.write(2, 0, "File Name")
-                ws.write(2, 1, "File Created Time")
-                ws.write(2, 2, "Days passed in between")
+                ws.write(2, 1, "File Size")
+                ws.write(2, 2, "File Created Time")
+                ws.write(2, 3, "Last Modified Time")
+                ws.write(2, 4, "Last Accessed Time")
+                ws.write(2, 5, "Days passed in between")
                 for i in range(len(time_list)):
                     ws.write(i+3, 0, file_list[i])
-                    ws.write(i+3, 1, ctime(time_list[i]))
+                    ws.write(i+3, 1, "{0} bytes".format(size_list[i]))
+                    ws.write(i+3, 2, ctime(time_list[i]))
+                    ws.write(i+3, 3, mod_list[i])
+                    ws.write(i+3, 4, access_list[i])
                     ago = today - d.datetime.fromtimestamp(time_list[i])
                     if ago.days:
-                        ws.write(i+3, 2, "{0} days".format(ago.days))
+                        ws.write(i+3, 5, "{0} days".format(ago.days))
                     else:
-                        ws.write(i+3, 2, "{0} seconds".format(ago.seconds))
+                        ws.write(i+3, 5, "{0} seconds".format(ago.seconds))
                 save_path = input("Enter the save directory                     : ")
                 while True:
                     try:
